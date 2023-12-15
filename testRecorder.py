8cf90fca4f8e3b476c1f36afa14988c81e5dca81
aoao0921 <2197168002@qq.com>
2023-12-15 14:37:14 +0800

    添加测试文件

---
+++ testRecorder.py
@@ -0,0 +1,19 @@
+# 导入需要的模块
+from pywinauto_recorder import Recorder
+from pywinauto_recorder.player import playback
+import time
+
+if __name__ == '__main__':
+# 创建Recorder对象
+	recorder = Recorder()
+
+# 开始录制
+	recorder.start_recording()
+
+	time.sleep(8)
+	filename = recorder.stop_recording()
+
+	print(filename)
+
+# 退出Recorder
+	recorder.quit()
